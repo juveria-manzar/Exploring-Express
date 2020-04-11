7dccbb62af47866b80288005c9a06e73d2bc0a16
Juveria Manzar <juveriadot@gmail.com>
2020-04-11 20:20:29 +0530

    post-request

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,18 @@ router.get('/tickets', function(req, res, next) {
 router.get('/stats', function(req, res, next) {
     res.render('stats', { title: 'Stats' });
 });
+router.get('/register', function(req, res) {
+    res.render('register_form', { title: 'Register' })
+})
+router.post('/register', function({ body }, res) {
+    console.log(body)
+    let submissionData = {
+        message: "the user registered with these credentials",
+        credentials: {
+            username: body.username,
+            password: body.password
+        }
+    }
+    res.json(submissionData)
+})
 module.exports = router;
