fbde27881fe4da33fcbde4758b1a67021d01757c
Juveria Manzar <juveriadot@gmail.com>
2020-04-11 19:08:03 +0530

    routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
 });
-
+router.get('/shop', function(req, res, next) {
+    res.render('shop', { title: 'Shop' });
+});
+router.get('/tickets', function(req, res, next) {
+    res.render('tickets', { title: 'Tickets' });
+});
+router.get('/stats', function(req, res, next) {
+    res.render('stats', { title: 'Stats' });
+});
 module.exports = router;
