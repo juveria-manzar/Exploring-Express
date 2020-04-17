a978b157188860627648b8949c4922ae474f15db
Juveria Manzar <juveriadot@gmail.com>
2020-04-17 22:44:52 +0530

    mvc-arch

--- mvc/controllers/posts.js
+++ mvc/controllers/posts.js
@@ -1,3 +1,11 @@
 const getPosts = function(req, res) {
-    res.render()
+    res.render('post_templates/posts', { title: 'All posts' })
+}
+const getPost = function({ params }, res) {
+    res.render('post_templates/post', { title: 'All posts', id: params.postid })
+}
+
+module.exports = {
+    getPosts,
+    getPost
 }
--- mvc/routes/posts.js
+++ mvc/routes/posts.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const ctrlPosts = require('../controllers/posts')
+
+router.get('/', ctrlPosts.getPosts)
+router.get('/:postid', ctrlPosts.getPost)
 module.exports = router;
